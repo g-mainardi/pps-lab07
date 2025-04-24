7b3ccad0bfbbc6df09d2b78dcf1d5c586f2a55b4
g-mainardi <giosue.mainardi@studio.unibo.it>
2025-04-24 17:19:26 +0200

    Ex 3: Solitaire type refactor

--- src/main/scala/ex3/Solitaire.scala
+++ src/main/scala/ex3/Solitaire.scala
@@ -1,15 +1,27 @@
 package ex3
 
 object Solitaire extends App:
-  def render(solution: Seq[(Int, Int)], width: Int, height: Int): String =
+  object Elements {
+    opaque type Board = (Int, Int)
+    def board(width: Int, height: Int): Board = (width, height)
+    extension (b: Board)
+      def width: Int = b match
+        case (w, _) => w
+      def height: Int = b match
+        case (_, h) => h
+    type Position = (Int, Int)
+    type Solution = Seq[Position]
+  }
+  import Elements.*
+  def render(solution: Solution, b: Board): String =
     val reversed = solution.reverse
     val rows =
-      for y <- 0 until height
-          row = for x <- 0 until width
+      for y <- 0 until b.height
+          row = for x <- 0 until b.width
           number = reversed.indexOf((x, y)) + 1
           yield if number > 0 then "%-2d ".format(number) else "X  "
       yield row.mkString
     rows.mkString("\n")
 
 
-  println(render(solution = Seq((0, 0), (2, 1)), width = 3, height = 3))
+  println(render(solution = Seq((0, 0), (2, 1)), board(width = 3, height = 3)))
